35d396033ebe8bdb74639b64caa9ad6d979b2336
IMhongkeun <hongeun15@gmail.com>
2026-04-08 21:15:49 +0900

    Add sitemap generator and automate dist robots/sitemap output

---
+++ sitemap-paths.js
@@ -0,0 +1,17 @@
+const sitemapPaths = [
+  '/',
+  '/bsa',
+  '/gdp',
+  '/heparin',
+  '/predicted-hct',
+  '/lbm',
+  '/timecalc',
+  '/do2i',
+  '/phn-echo',
+  '/quick-reference',
+  '/faq',
+  '/contact',
+  '/info'
+];
+
+module.exports = sitemapPaths;
