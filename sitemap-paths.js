fdbe6cfb9380cc200a9effa3a6bc27dbd2982ec0
IMhongkeun <hongeun15@gmail.com>
2026-04-09 21:35:36 +0900

    Add requested SEO title/description tags for remaining calculators

---
+++ sitemap-paths.js
@@ -0,0 +1,19 @@
+const sitemapPaths = [
+  '/',
+  '/bsa',
+  '/gdp',
+  '/heparin',
+  '/predicted-hct',
+  '/lbm',
+  '/timecalc',
+  '/do2i',
+  '/phn-echo',
+  '/quick-reference',
+  '/faq',
+  '/contact',
+  '/info',
+  '/priming-volume',
+  '/unit-converter'
+];
+
+module.exports = sitemapPaths;
